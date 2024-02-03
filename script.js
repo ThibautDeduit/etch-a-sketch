51d1cab8dc49c0fd30b261811bd4619781db7382
ThibautDeduit <149255011+ThibautDeduit@users.noreply.github.com>
2024-02-03 21:16:15 +0100

    Allow to reset grid on click

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
-setGrid(16, 16)
+let gridContainer = document.querySelector("#gridContainer")
+
+setGrid(16)
 activateCells()
 
-function setGrid(width, height) {
-	let gridContainer = document.querySelector("#gridContainer")
-	for (let i = 0; i < width; i++) {
+function setGrid(squaresPerSide) {
+	for (let i = 0; i < squaresPerSide; i++) {
 		let gridColumn = document.createElement("div")
 		gridColumn.classList.add("gridColumn")
-		for (let j = 0; j < height; j++) {
+		for (let j = 0; j < squaresPerSide; j++) {
 			let gridCell = document.createElement("div")
 			gridCell.classList.add("gridCell")
 			gridColumn.appendChild(gridCell)
 		}
 		gridContainer.appendChild(gridColumn)
 	}
+	console.log("setGrid")
 }
 
 function activateCells() {
-	let wakeCells = document.querySelectorAll(".gridCell")
-	wakeCells.forEach((cell) => {
+	let cells = document.querySelectorAll(".gridCell")
+	cells.forEach((cell) => {
 		cell.addEventListener("mouseenter", (e) => {
 			e.target.style.backgroundColor = "black"
 		})
 	})
+}
+
+document.body.addEventListener("click", promptForReset)
+
+function promptForReset() {
+	if (confirm("Do you want to reset the grid?")) {
+		let squaresPerSide = prompt("How many squares per side do you want?")
+		while (
+			!Number(squaresPerSide) ||
+			squaresPerSide < 1 ||
+			squaresPerSide > 100) {
+				squaresPerSide = prompt("How many squares per side do you want?")
+		}
+		console.log(squaresPerSide)
+		squaresPerSide && resetGrid(squaresPerSide)
+	}
+}
+
+function resetGrid(squaresPerSide) {
+	console.log("resetGrid")
+	gridContainer.textContent = ""
+	setGrid(squaresPerSide)
+	activateCells()
 }
